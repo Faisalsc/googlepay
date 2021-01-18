97781de37f4bf32b1e03d1024bfe01a74a06fea6
faisal <faisal.m@pogeyan.com>
2021-01-18 11:35:56 +0530

    want to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
-
+import GooglePayButton from "@google-pay/button-react";
+import React from "react"
 function App() {
+  const allowedNetworks = ['VISA','MASTERCARD'];
+const allowedAuthMethods = ['PAN_ONLY','CRYPTOGRAM_3DS'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>google pay integration</h1>
+      <GooglePayButton
+        environment="TEST"
+        paymentRequest={{
+          apiVersion: 2,
+          apiVersionMinor: 0,
+          allowedPaymentMethods: [
+            {
+              type: 'CARD',
+              parameters: {
+                allowedCardNetworks: allowedNetworks,
+                allowedAuthMethods: allowedAuthMethods,
+                // allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                // allowedCardNetworks: ['MASTERCARD', 'VISA'],
+              },
+              tokenizationSpecification: {
+                type: 'PAYMENT_GATEWAY',
+                parameters: {
+                  gateway: 'example',
+                  gatewayMerchantId: 'exampleGatewayMerchantId',
+                },
+              },
+            },
+          ],
+          merchantInfo: {
+            merchantId: 'BCR2DN6TSPJ7XKYY',
+            merchantName: 'Demo Merchant',
+          },
+          transactionInfo: {
+            totalPriceStatus: 'FINAL',
+            totalPriceLabel: 'Total',
+            totalPrice: '1',
+            currencyCode: 'USD',
+            countryCode: 'US',
+          },
+          shippingAddressRequired: true,
+          callbackIntents: ['SHIPPING_ADDRESS', 'PAYMENT_AUTHORIZATION'],
+        }}
+        onLoadPaymentData={paymentRequest => {
+          console.log('Success', paymentRequest);
+        }}
+        onPaymentAuthorized={paymentData => {
+          console.log('Payment Authorised Success', paymentData)
+          return { transactionState: 'SUCCESS' }
+        }
+        }
+        onPaymentDataChanged={paymentData => {
+          console.log('On Payment Data Changed', paymentData)
+          return {}
+        }
+        }
+        existingPaymentMethodRequired='false'
+        buttonColor='black'
+        buttonType='Buy'
+      />
     </div>
   );
 }
